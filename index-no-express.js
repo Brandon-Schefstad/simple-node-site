159503391980bd7fcb7709271418960949860e64
Brandon-Schefstad <bschefstad@gmail.com>
2022-11-29 11:38:16 -0500

    EJS Dynamic Rendering

---
+++ index-no-express.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const app = require('express');
+const hostname = '0.0.0.0';
+const port = 300;
+
+/* Reusable function to serve a specific file in views. 
+		@PARAMS: fileName - Name of the file
+		@PARAMS: res - response object from createServer()*/
+
+function fetchPage(fileName, res) {
+	//
+	fs.readFile(`./views/${fileName}.html`, (err, data) => {
+		if (err) {
+			console.error(err);
+			res.end();
+		} else {
+			// res.write(data);
+			res.end(data);
+		}
+	});
+}
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	// Essential Set Header Content Type
+	res.setHeader('Content-Type', 'text/html');
+
+	switch (req.url) {
+		case '/':
+			fetchPage('index', res);
+			break;
+		case '/about':
+			fetchPage('about', res);
+			break;
+		case '/contact-me':
+			fetchPage('contact-me', res);
+			break;
+		default:
+			fetchPage('404', res);
+			break;
+	}
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-const http = require('http');
-const fs = require('fs');
-const hostname = '0.0.0.0';
-const port = 300;
+const express = require('express');
 
-/* Reusable function to serve a specific file in views. 
-		@PARAMS: fileName - Name of the file
-		@PARAMS: res - response object from createServer()*/
+const app = express();
 
-function fetchPage(fileName, res) {
-	//
-	fs.readFile(`./views/${fileName}.html`, (err, data) => {
-		if (err) {
-			console.error(err);
-			res.end();
-		} else {
-			// res.write(data);
-			res.end(data);
-		}
-	});
-}
+// PORT
+app.listen(3000);
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	// Essential Set Header Content Type
-	res.setHeader('Content-Type', 'text/html');
+// Declare View Enginer
+app.set('view engine', 'ejs');
 
-	switch (req.url) {
-		case '/':
-			fetchPage('index', res);
-			break;
-		case '/about':
-			fetchPage('about', res);
-			break;
-		case '/contact-me':
-			fetchPage('contact-me', res);
-			break;
-		default:
-			fetchPage('404', res);
-			break;
-	}
+// Sample html route
+// app.get('/', (req, res) => {
+// 	const path = __dirname + '/views/index.html';
+// 	res.sendFile(path); // Optional 2nd param of { root: __dirname}
+// });
+
+// redirect
+// app.get('/about-us', (req, res) => {
+// 	res.redirect('/about');
+// });
+
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+app.get('/contact-me', (req, res) => {
+	res.render('contact-me');
+});
+
+app.get('/blogs/create', (req, res) => {
+	res.render('create');
+});
+app.get('/blogs/create/new', (req, res) => {
+	const { name, number, letter } = req.query;
+	console.log(name);
+	console.log(number);
+	console.log(letter);
+	res.render('create', {
+		number,
+		name,
+		letter,
+	});
+});
+
+// Middleware
+app.use((req, res) => {
+	res.render('404');
 });
