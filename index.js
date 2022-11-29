0140e0ac612d4f9e614603b905dfbad101492d01
Brandon-Schefstad <bschefstad@gmail.com>
2022-11-29 06:36:57 -0500

    removing absolute path

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 const http = require('http');
 const fs = require('fs');
 const hostname = 'localhost';
-const port = 3001;
+const port = 300;
 
-const startPath = '/Users/bsche/onedrive/desktop/2nd brain/projects/nodeJS';
+/* Reusable function to serve a specific file in views. 
+		@PARAMS: fileName - Name of the file
+		@PARAMS: res - response object from createServer()*/
+
+function fetchPage(fileName, res) {
+	//
+	fs.readFile(`./views/${fileName}.html`, (err, data) => {
+		if (err) {
+			console.error(err);
+			res.end();
+		} else {
+			// res.write(data);
+			res.end(data);
+		}
+	});
+}
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method);
 	res.statusCode = 200;
 	// Essential Set Header Content Type
 	res.setHeader('Content-Type', 'text/html');
-	function fetchPage(fileName) {
-		fs.readFile(
-			startPath + `/simpleNodeSite/views/${fileName}.html`,
-			(err, data) => {
-				if (err) {
-					console.error(err);
-					res.end();
-				} else {
-					res.write(data);
-					res.end();
-				}
-			}
-		);
-	}
+
 	switch (req.url) {
 		case '/':
-			fetchPage('index');
+			fetchPage('index', res);
 			break;
 		case '/about':
-			fetchPage('about');
+			fetchPage('about', res);
 			break;
 		case '/contact-me':
-			fetchPage('contact-me');
+			fetchPage('contact-me', res);
 			break;
 		default:
-			fetchPage('404');
+			fetchPage('404', res);
 			break;
 	}
 });
